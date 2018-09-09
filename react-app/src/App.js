1d38eab2ec25e98a6b4d041f03fb2311fc45000e
Alex Ang <E0032221@u.nus.edu>
2018-09-09 04:15:36 -0400

    Update app
    add levenshtein distance utility

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -11,6 +11,26 @@ const APP_ID = "auto-linguo-xaqwa";
 const client = Stitch.initializeDefaultAppClient(APP_ID);
 const mongo = client.getServiceClient(RemoteMongoClient.factory, "mongodb-atlas");
 
+/**
+ * Returns the Levenshtein distance between two strings
+ * {@param a} and {@param b}.
+ *
+ * {@author http://rosettacode.org/wiki/Levenshtein_distance}
+ */
+const levenshteinDistance = (a, b) => {
+    a = a.toLowerCase();
+    b = b.toLowerCase();
+    let t = [], u, i, j, m = a.length, n = b.length;
+    if (!m) { return n; }
+    if (!n) { return m; }
+    for (j = 0; j <= n; j++) { t[j] = j; }
+    for (i = 1; i <= m; i++) {
+        for (u = [i], j = 1; j <= n; j++) {
+            u[j] = a[i - 1] === b[j - 1] ? t[j - 1] : Math.min(t[j - 1], t[j], u[j - 1]) + 1;
+        } t = u;
+    } return u[n];
+};
+
 const words = ["Bonjour", "Salut", "Merci", "Je", "Tu", "Suis", "Oui", "Non" ];
 
 class App extends Component {
