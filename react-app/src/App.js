7df71344dfbc1827723d7c850aebc90bdeedec8d
Noah Lattari <zennoah1998@gmail.com>
2018-09-08 23:42:57 -0400

    Added select

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import franceBackgroundImage from './france_background.jpg';
+import settingsIcon from './settings_icon.png';
 import logo from './logo.svg';
 import './App.css';
 
@@ -19,7 +20,8 @@ const getRandomWord = () => {
 class App extends Component {
   state = {
     wordToShow: null,
-    backgroundImgUrl: franceBackgroundImage
+    backgroundImgUrl: franceBackgroundImage,
+    languageChoice: "french"
   };
 
   componentDidMount() {
@@ -28,6 +30,14 @@ class App extends Component {
     this.setState({
       wordToShow: word
     });
+  }
+
+  saveChanges = () => {
+    console.log("The selected language is", this.state.languageChoice);
+  }
+
+  selectLanguage = (event) => {
+    this.setState({ selectLanguage: event.target.value });
   }
 
   render() {
@@ -42,6 +52,30 @@ class App extends Component {
           {this.state.wordToShow} -> {this.state.wordToShow}
         </p>
         <p className="sub-paragraph"> Open a new tab or refresh for a different word. </p>
+        <img className = "settings" src={settingsIcon} data-toggle="modal" data-target="#settingsModal" />
+        <div className="modal fade" id="settingsModal" tabIndex="-1" role="dialog" aria-labelledby="setting sModalLabel" aria-hidden="true">
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="settingsModalLabel">Language Preferences</h5>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <select onChange={this.selectLanguage}>
+                  <option id="french">French</option>
+                  <option id="spanish">Spanish</option>
+                </select>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-primary" onClick={this.saveChanges}>Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     );
   }
