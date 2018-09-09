ba821fe8b9f2f0d406f02fa671a558f39d198908
Alex Ang <E0032221@u.nus.edu>
2018-09-08 21:53:10 -0400

    Update background script
    Refactor es5

--- background.js
+++ background.js
@@ -1,10 +1,8 @@
 // TODO: Script replace in react app:
 // chrome-extension://lgghdoicfikanikdfnffdfiogobiaaek/react-app/build/static/js/main.87c135b1.js
 
-chrome.getExtensionId()
+const getExtensionId = () => chrome.runtime.id;
 
-(function () {
-    chrome.browserAction.onClicked.addListener(function(tab) {
-        chrome.tabs.create({url: ""})
-    })
+chrome.browserAction.onClicked.addListener((tab) => {
+    chrome.tabs.create({url: chrome.runtime.getURL("react-app/build/index.html")});
 });
