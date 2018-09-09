68d00357a0bf985ade08a2898b6bef10144f20a9
Alex Ang <E0032221@u.nus.edu>
2018-09-08 23:52:09 -0400

    Update app
    execute auth

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -15,7 +15,7 @@ const languages = {
         description: "add a ",
         imageUrl: ""
     }
-    
+
 };
 
 const getRandomWord = () => {
@@ -24,67 +24,85 @@ const getRandomWord = () => {
 };
 
 class App extends Component {
-  state = {
-    wordToShow: null,
-    backgroundImgUrl: franceBackgroundImage,
-    languageChoice: "french"
-  };
+    state = {
+        wordToShow: null,
+        backgroundImgUrl: franceBackgroundImage,
+        languageChoice: "french"
+    };
 
-  componentDidMount() {
-    const word = getRandomWord();
-    console.log(word);
-    this.setState({
-      wordToShow: word
-    });
-  }
+    componentDidMount() {
+        const word = getRandomWord();
+        console.log(word);
+        this.setState({
+            wordToShow: word
+        });
+    }
 
-  saveChanges = () => {
-    console.log("The selected language is", this.state.languageChoice);
-  }
+    saveChanges = () => {
+        console.log("The selected language is", this.state.languageChoice);
+    };
 
-  selectLanguage = (event) => {
-    this.setState({ selectLanguage: event.target.value });
-  }
+    selectLanguage = (event) => {
+        this.setState({ selectLanguage: event.target.value });
+    };
 
-  render() {
-    return (
-      <div className="App" style={{
-        backgroundImage: `url('${this.state.backgroundImgUrl}')`
-      }}>
-        <header className="App-header">
-          <h1 className="App-title">Learn: <u>French</u>!</h1>
-        </header>
-        <p className="App-intro">
-          {this.state.wordToShow} -> {this.state.wordToShow}
-        </p>
-        <p className="sub-paragraph"> Open a new tab or refresh for a different word. </p>
-        <img className = "settings" src={settingsIcon} data-toggle="modal" data-target="#settingsModal" />
-        <div className="modal fade" id="settingsModal" tabIndex="-1" role="dialog" aria-labelledby="setting sModalLabel" aria-hidden="true">
-          <div className="modal-dialog" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="settingsModalLabel">Language Preferences</h5>
-                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div className="modal-body">
-                <select onChange={this.selectLanguage}>
-                  <option id="french">French</option>
-                  <option id="spanish">Spanish</option>
-                </select>
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" className="btn btn-primary" onClick={this.saveChanges}>Save changes</button>
-              </div>
+    execute = event => {
+        client.auth
+            .loginWithCredential(new AnonymousCredential())
+            .then(user => {
+                console.log("Logged in to mongodb as user: ", user);
+                const frenchCollection = mongo.db("dictionary").collection("french");
+                return frenchCollection.insertMany(words);
+            })
+            .then(results => {
+                console.log("Inserted words into french dictionary with results: ", results);
+            })
+            .catch(console.error);
+        if (event) {
+            event.preventDefault();
+        }
+    };
+
+    render() {
+        return (
+            <div className="App" style={{
+                backgroundImage: `url('${this.state.backgroundImgUrl}')`
+            }}>
+                <header className="App-header">
+                    <h1 className="App-title">Learn: <u>French</u>!</h1>
+                </header>
+                <p className="App-intro">
+                    {this.state.wordToShow} -> {this.state.wordToShow}
+                </p>
+                <button onClick={this.execute}>Auth test</button>
+                <p className="sub-paragraph"> Open a new tab or refresh for a different word. </p>
+                <img className = "settings" src={settingsIcon} data-toggle="modal" data-target="#settingsModal" />
+                <div className="modal fade" id="settingsModal" tabIndex="-1" role="dialog" aria-labelledby="setting sModalLabel" aria-hidden="true">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="settingsModalLabel">Language Preferences</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                <select onChange={this.selectLanguage}>
+                                    <option id="french">French</option>
+                                    <option id="spanish">Spanish</option>
+                                </select>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" className="btn btn-primary" onClick={this.saveChanges}>Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
-          </div>
-        </div>
-
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
